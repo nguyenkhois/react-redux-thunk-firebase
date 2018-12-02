73ab1d360348da07ece1bd12729b3d744748e378
Khoi Le <minarek@outlook.com>
2018-12-02 09:16:07 +0100

    Edited comments

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import FetchData from './fetchdata';
+import FetchData from './components/fetchdata';
 
 export default class App extends Component {
     render(){
--- src/components/fetchdata.js
+++ src/components/fetchdata.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { dbActions } from './actions/firebase-action';
+import { dbActions } from '../actions/firebase-action';
 
 const mapDispatchToProps = {
     fetchData: dbActions.fetchData,

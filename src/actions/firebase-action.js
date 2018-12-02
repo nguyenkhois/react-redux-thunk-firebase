0ae876e915739e9bf5a5fe705166a2eac5396a2b
Khoi Le <minarek@outlook.com>
2018-12-02 09:12:10 +0100

    Improved code

--- src/actions/firebase-action.js
+++ src/actions/firebase-action.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-const dataRef = firebase.database().ref('/');
+const dataRef = firebase.database().ref('/demo');
 
+/**
+ * 1. We must use the composition function because we have been using redux-thunk.
+ * 2. Our input data is the first argument.
+ * 3. redux-thunk care about the second argument.
+ */
 export const dbActions = {
     fetchData: () => async (dispatch) => {
+        // Dispatch an action with async syntax
         dataRef.on('value', (snapshot) => {
             dispatch({
                 type: 'FETCH_DATA',
@@ -12,7 +18,10 @@ export const dbActions = {
             });
         });
     },
+    
     addData: (data) => async () => {
+        // Do not use redux-thunk dispatch when we don'n need it.
+        // This is only a normal Firebase syntax.
         dataRef.push().set(data);
     }
 };
--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 
 const firebaseConfig = {
     // Your Firebase key is here
+    
 };
 
 firebase.initializeApp(firebaseConfig);
